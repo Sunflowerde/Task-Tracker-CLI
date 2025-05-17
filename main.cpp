9864d9aea5b341ab420b844f16430e62cd99d952
sunflowerde <2410306105@stu.pku.edu.cn>
2025-05-17 16:46:28 +0800

    Finished loadTasks.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 struct Task {
   int id; // 用于表示每个任务的编号
@@ -14,8 +15,59 @@ struct Task {
   }
 };
 
-int main() {
-  Task t = {1, "do homework", "todo"};
-  std::cout << t.toJson() << std::endl;
-  return 0;
+// 解析 json 文件
+std::vector<Task> loadTasks(const std::string& filename) {
+  // 打开名为 filename 的文件，存储在 file 中方便后续操作
+  std::ifstream file(filename);
+  std::string json;
+  std::vector<Task> tasks;
+
+  // 无法打开文件的情形
+  if (!file.is_open()) {
+    std::cerr << "Failed to open file: " << filename << std::endl;
+    return tasks;
+  }
+
+  // 将 file 中的内容读取到 json 中，直到出现文字终止符 '\0'
+  std::getline(file, json, '\0');
+
+  size_t pos = 0;
+  // 从 json 中找到第一个，将索引赋值给 pos，如果没有找到的话，find 返回值即为 std::string::npos
+  // 这样循环就能读取 json 中所有的任务
+  while ((pos = json.find("{")) != std::string::npos) {
+    Task task;
+    json = json.substr(pos + 1); // 从 { 后的子串开始解析
+    task.id = std::stoi(json.substr(json.find(":") + 1, json.find(","))); // std::stoi 将字符串转换为整数，读取 ":" 和 "," 之间的 id
+    json = json.substr(json.find("description") + 13); // find 返回值为 d 的索引，跳过 10 个字母，"，:， 得到13
+    task.description = json.substr(0, json.find("\""));
+    json = json.substr(json.find("status") + 9);
+    task.status = json.substr(0, json.find("\""));
+    tasks.push_back(task);
+    json = json.substr(json.find("}") + 1);
+  }
+  return tasks;
+}
+
+int main(int argc, char *argv[]) {
+  // 输入参数过少
+  if (argc < 2) {
+    std::cout << "Usage: task-cli <command> [arguments...]" << std::endl;
+    return 1;
+  }
+
+  std::string command = argv[1];
+  if (command == "add") {
+    // 未提供 description 参数
+    if (argc == 2) {
+      std::cout << "Error: No description provided." << std::endl;
+      return 1;
+    }
+
+    // 添加任务
+    std::string description = argv[2];
+    for (int i = 3; i < argc; ++i) {
+      description += " ";
+      description += argv[i];
+    }
+  }
 }
